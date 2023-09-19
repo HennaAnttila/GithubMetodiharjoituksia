417665f9fc6aa8bed58c516412558bc45e83b71d
Henna <hennariina23000@student.hamk.fi>
2023-09-19 20:53:35 +0300

    Tehtävät 1 ja 2

--- src/GithubMetodiharjoituksia.java
+++ src/GithubMetodiharjoituksia.java
@@ -2,8 +2,29 @@
 public class GithubMetodiharjoituksia {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
-	}
+	//kutsu tehtävän 1 metodia
+		tulostus();
+		
+	//kutsu tehtävän 2 metodia
+		tulo(10);
+
+	}// mainin loppu
+	
+	
+	//1. Tee metodi, joka tulostaa "Metodi-harjoituksia"
+		public static void tulostus()
+		{
+			System.out.println("Metodi-harjoituksia");
+		}//tulostus-metodin loppu
+		
+		
+	//2. Tee metodi, joka kertoo parametrina saadun luvun tuhannella ja tulostaa tulon
+		public static void tulo(int luku)
+		
+		{
+			System.out.println(luku * 1000);
+		}//tulo-metodin loppu
 
 }
