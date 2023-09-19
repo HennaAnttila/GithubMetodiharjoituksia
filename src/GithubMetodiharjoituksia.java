fed5ae1014af51988ef9facffff8025eb170130f
Henna <hennariina23000@student.hamk.fi>
2023-09-19 21:01:07 +0300

    Tehtävät 3 ja 4

--- src/GithubMetodiharjoituksia.java
+++ src/GithubMetodiharjoituksia.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 public class GithubMetodiharjoituksia {
 
 	public static void main(String[] args) {
@@ -9,6 +9,12 @@ public class GithubMetodiharjoituksia {
 		
 	//kutsu tehtävän 2 metodia
 		tulo(10);
+		
+	//kutsu tehtävän 3 metodia
+		kysymys();
+	
+	//kutsu tehtävän 4 metodia
+		pintaAla(10, 5);
 
 	}// mainin loppu
 	
@@ -26,5 +32,27 @@ public class GithubMetodiharjoituksia {
 		{
 			System.out.println(luku * 1000);
 		}//tulo-metodin loppu
+		
+		
+	//3. Metodi kysyy käyttäjältä luvun ja kutsuu sitten tulo-metodia.
+		public static void kysymys()
+		
+		{
+			Scanner in = new Scanner(System.in);
+			String vastaus;
+			System.out.println("Anna minulle jokin luku.");
+			vastaus = in.nextLine();
+			int vastausInt = Integer.parseInt(vastaus);
+			
+			tulo(vastausInt);
+		}//kysymys-metodin loppu
+		
+		
+	//4. Tee metodi, joka laskee pinta-alan. Pyydä pituus ja leveys mainissä ja kutsu metodia. 
+		public static void pintaAla(int pituus, int leveys)
+		
+		{
+			System.out.println(pituus * leveys);
+		}//pintaAla-metodin loppu
 
 }
